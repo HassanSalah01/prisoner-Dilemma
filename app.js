3cac5d9b5023fc2ff6aa2d9c13dddb947452b3a9
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2024-01-29 22:29:22 +0200

    added-main-function

---
+++ app.js
@@ -0,0 +1,16 @@
+const { Player } = require("./Player");
+
+const main = () => {
+  const playerNumber = 10;
+  for (let i = 0; i < playerNumber.length; i++) {}
+};
+main();
+
+p1.setAnswer(p2.answer);
+
+const TFTT = (p1, p2) => {
+  const p1 = new Player(1);
+  const p2 = new Player(0);
+  const p1 = new PlayerAlgo(1, "TFTT");
+  const p1 = new PlayerAlgo(1, "bassam");
+};
